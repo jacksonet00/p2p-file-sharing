ffbc1ff5529af56cc68eb6199e27e1bac35ee374
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-10-18 16:21:07 -0400

    add Peer.connectToPeers

--- src/Peer.java
+++ src/Peer.java
@@ -1,15 +1,38 @@
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.List;
+
 public class Peer {
 
     public int id;
     public String hostname;
-    int listeningPort;
-    boolean hasFile;
+    public int listeningPort;
+    public boolean hasFile;
 
     public Peer(int id, String hostname, int listeningPort, boolean hasFile) {
         this.id = id;
         this.hostname = hostname;
         this.listeningPort = listeningPort;
         this.hasFile = hasFile;
+    }
+
+    public void init(){ 
+        // start listener
+    }
+
+    public void connectToPeers(List<Peer> peers) throws UnknownHostException, IOException {
+        int peerIndex = 0;
+        while (!peers.isEmpty()) {
+            try {
+                Socket socket = new Socket(peers.get(peerIndex).hostname, peers.get(peerIndex).listeningPort);
+            }
+            catch (ConnectException e) {
+                peerIndex++;
+            }
+
+        }
     }
     
 }
