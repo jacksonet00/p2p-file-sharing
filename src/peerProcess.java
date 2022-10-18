4dd4fefa68451059bfb6795e29c652e8db6afa8b
Nalin Mehra <nalinm01@gmail.com>
2022-10-18 16:17:34 -0400

    add client class and read "Common.cfg"

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -1,26 +1,36 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Hashtable;
 
 public class peerProcess {
     
     public static void main(String args[]) throws FileNotFoundException {
 
-        Hashtable<Integer, Peer> peerTable = new Hashtable<Integer, Peer>();
-
         File peerInfoConfig = new File("PeerInfo.cfg");
-        Scanner scanner = new Scanner(peerInfoConfig);
+        File commonConfig = new File("Common.cfg");
+       
+        Scanner commonScanner = new Scanner(commonConfig);
+        // Read in Client parameters from Common cfg
+        int numPreferredNeighbors = Integer.parseInt(commonScanner.nextLine().split(" ")[1]);
+        int unchokingInterval = Integer.parseInt(commonScanner.nextLine().split(" ")[1]);
+        int optimisticUnchokingInterval = Integer.parseInt(commonScanner.nextLine().split(" ")[1]);
+        String fileName = commonScanner.nextLine().split(" ")[1];
+        long fileSize = Long.parseLong(commonScanner.nextLine().split(" ")[1]);
+        int pieceSize = Integer.parseInt(commonScanner.nextLine().split(" ")[1]);
+        commonScanner.close();
 
-        while (scanner.hasNextLine()) {
-            String record = scanner.nextLine();
+        Client client = new Client(numPreferredNeighbors, unchokingInterval, optimisticUnchokingInterval, fileName, fileSize, pieceSize);
+
+        Scanner peerInfoScanner = new Scanner(peerInfoConfig);
+        // Initialize peer table by reading PeerInfo.cfg
+        while (peerInfoScanner.hasNextLine()) {
+            String record = peerInfoScanner.nextLine();
             String[] entries = record.split(" ");
 
             Peer peer = new Peer(Integer.parseInt(entries[0]), entries[1], Integer.parseInt(entries[2]), Integer.parseInt(entries[3]) == 1);
 
-            peerTable.put(peer.id, peer);
+            client.peerTable.put(peer.id, peer);
         }
-
-        scanner.close();
+        peerInfoScanner.close();
     }
 }
