d201eb70097981b3eaeae04578037859c02ae49b
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-10-21 17:18:58 -0400

    implemented logger

---
+++ src/Logger.java
@@ -0,0 +1,121 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Logger {
+
+    public class LoggerUtil {
+
+        public static String idListToCsv(List<Integer> idList) {
+            StringBuilder csv = new StringBuilder();
+
+            for (int i = 0; i < idList.size() - 1; i++) {
+                csv.append(Integer.toString(idList.get(i)) + ", ");
+            }
+            csv.append(Integer.toString(idList.get(idList.size() - 1)));
+
+            return csv.toString();
+        }
+    }
+    
+    public static void logTcpConnectionIniated(int srcPeerId, int destPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] makes a connection to Peer ["
+            + Integer.toString(destPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logTcpConnectionIncoming(int srcPeerId, int destPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] is connected from Peer ["
+            + Integer.toString(destPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logChangePerferredNeighbors(int peerId, List<Integer> preferredNeighborIdList) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(peerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(peerId) + "] has the preferred neighbors ["
+            + LoggerUtil.idListToCsv(preferredNeighborIdList) + "].\n");
+        logger.close();
+    }
+
+    public static void logChangeOptimisticallyUnchokedNeighbor(int srcPeerId, int neighborPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] has the optimistically unchoked neighbor ["
+            + Integer.toString(neighborPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logUnchokedNeighbor(int srcPeerId, int neighborPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] is unchoked by ["
+            + Integer.toString(neighborPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logChokeNeighbor(int srcPeerId, int neighborPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] is choked by ["
+            + Integer.toString(neighborPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logReceiveHaveMessage(int srcPeerId, int destPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] received the ‘have’ message from ["
+            + Integer.toString(destPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logReceiveInterestedMessage(int srcPeerId, int destPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] received the ‘interested’ message from ["
+            + Integer.toString(destPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logNotReceiveInterestedMessage(int srcPeerId, int destPeerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] received the ‘not interested’ message from ["
+            + Integer.toString(destPeerId) + "].\n");
+        logger.close();
+    }
+
+    public static void logDownloadedPiece(int srcPeerId, int destPeerId, int pieceId, int pieceCount) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(srcPeerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(srcPeerId) + "] has downloaded the piece"
+            + Integer.toString(pieceId) + "] from ["
+            + Integer.toString(destPeerId) + "]. Now the number of pieces it has is ["
+            + Integer.toString(pieceCount) + "].");
+        logger.close();
+    }
+
+    public static void logDownloadComplete(int peerId) throws IOException {
+        FileWriter logger = new FileWriter("./logs/log_peer_" + Integer.toString(peerId) + ".log");
+
+        logger.write("[" + LocalDateTime.now() + "]: Peer [" 
+            + Integer.toString(peerId) + "] has downloaded the complete file.");
+        logger.close();
+    }
+}
