8b952179f268400074b0de485863ebc1612aa6f2
Nalin Mehra <nalinm01@gmail.com>
2022-12-06 17:35:14 -0500

    begin piece retrieval logic

--- src/MessagingService.java
+++ src/MessagingService.java
@@ -39,13 +39,16 @@ public class MessagingService implements Runnable {
             while (true) {
                     byte [] rawMessage = (byte[])_inputStream.readObject();
                     if (rawMessage[4] < 8) {
+                        ByteBuffer message =  ByteBuffer.wrap(rawMessage);
+                        int messagePayloadLength = message.getInt();
+                        byte messageType = message.get();
                         // Receiving bitfield message!
-                        if (rawMessage[4] == 0) {}
-                        else if (rawMessage[4] == 1) {}
-                        else if (rawMessage[4] == 2) {}
-                        else if (rawMessage[4] == 3) {}
-                        else if (rawMessage[4] == 4) {}
-                        else if(rawMessage[4] == 5) {
+                        if (messageType == 0) {}
+                        else if (messageType == 1) {}
+                        else if (messageType == 2) {}
+                        else if (messageType == 3) {}
+                        else if (messageType == 4) {}
+                        else if(messageType == 5) {
                             // TODO: decode bitfield, maybe reorganize how the messages are being read
                             // ran into issue with buffer underflow when a message other than the handshake was received cause of how we're reading in 0-18 bytes initially
                             // so I encased it in the else block, but there may be a better way to do it
@@ -57,8 +60,29 @@ public class MessagingService implements Runnable {
                             //  and then setting it here on handshake retrieve for the connection accepting case
                             System.out.println(_peer._id + " receives bitfield from " + _remotePeerId);
                         }
-                        else if (rawMessage[4] == 6) {}
-                        else if (rawMessage[4] == 7) {}
+                        else if (messageType == 6) {}
+                        else if (messageType == 7) {
+                            int pieceIndex = message.getInt();
+                            byte[] piece = new byte[message.remaining()];
+                            message.get(piece);
+                            // Store piece in peer's current pieces (downloading the piece per se)
+                            _peer._pieces.put(pieceIndex, piece);
+                            Logger.logDownloadedPiece(_peer._id, _remotePeerId, pieceIndex, _peer._pieces.size());
+                            // Update peer's bitfield to indicate piece retrieved
+                            _peer.setBitfield(pieceIndex, true);
+
+                            // send has piece message to all other pieces
+                            // TODO: may need to put this on its own thread..? current output stream is just sent to one socket connection (remote peer that it received the piece from)
+                            for(int key: _peer._connectedPeers.keySet()) {
+                                try {
+                                    byte[] haveMessage = MessageFactory.genHaveMessage(pieceIndex);
+                                    _peer.send(haveMessage, _outputStream, _remotePeerId);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            // TODO: send request message of random piece that we do NOT have
+                        }
                         else {
                             System.out.println("Message not of valid type");
                         }
